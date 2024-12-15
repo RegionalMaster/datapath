881045dd22a9073d89bf086ba58fc243e3ef2996
Abose Ukhun <bexis17@gmail.com>
2024-12-15 12:47:31 +0300

    Configure app for GitHub Pages deployment and fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/datapath">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/courses" element={<Courses />} />
