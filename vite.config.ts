2774f639a689e4235085bf3ba0fba4fb6a81553f
Abose Ukhun <bexis17@gmail.com>
2024-12-26 20:25:58 +0300

    Update base path and router basename to match datapath deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/data-navigator-magic/' : '/',
+  base: mode === 'production' ? '/datapath/' : '/',
   server: {
     host: "::",
     port: 8080,
